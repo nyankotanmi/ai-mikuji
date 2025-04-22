9a54668244670a453590322935d2c56618151ab4
nyankotanmi <nyanko.tanmi@gmail.com>
2025-04-22 21:31:32 +0900

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ function startExperience() {
   document.querySelector('.launch').style.display = 'none';
   document.querySelector('.experience').classList.remove('hidden');
 }
+
 document.getElementById('saveBtn').addEventListener('click', () => {
   alert('画像保存機能はここに入ります（html2canvas対応予定）');
 });
